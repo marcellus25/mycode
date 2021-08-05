2de105d70a3c1f366b05ebbee370f014a16ef17e
marcellus25 <88339936+marcellus25@users.noreply.github.com>
2021-08-05 18:55:23 +0000

    Json poke script update

--- JSON_API.py
+++ JSON_API.py
@@ -1,12 +1,12 @@
-
-API = "http://api.open-notify.org/astros.json"
-
+#!/usr/bin/env python3
 import requests
-
+api = "https://pokeapi.co/api/v2/pokemon/bulbasaur"
 def main():
-    resp = requests.get(API)
-    spacedata = resp.json()
-    print(spacedata)
-    print(f"There are currently {spacedata.get('number')} people in space.")
+    character = input ("What pokemon are you looking for? <input bulbasaur>").lower
+    resp = requests.get(api)
+    poke = resp.json()
+    print(poke)
+    #print(poke.keys())
 if __name__ == "__main__":
     main()
+
